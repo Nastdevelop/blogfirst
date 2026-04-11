d9b231bc679787648fd94cc0132a60f272a80ee1
Nastdevelop <nastdeveloper@gmail.com>
2026-04-11 18:19:44 +0700

    fixing prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,17 @@
-import { PrismaClient } from "../app/generated/prisma/client"; 
-import { PrismaPg } from "@prisma/adapter-pg"; 
+import { PrismaClient } from "@prisma/client";
+
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient; 
-}; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
-export const prisma =
+  prisma: PrismaClient;
+};
+
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
-export default prisma; 
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
