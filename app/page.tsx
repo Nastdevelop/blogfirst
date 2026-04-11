c72be194d005f49110d0b88447e574c1591f45e3
Nastdevelop <nastdeveloper@gmail.com>
2026-04-11 14:15:40 +0700

    gass

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   return (
     <div className="max-w-2xl mx-auto mt-10">
       <h1 className="text-2xl font-bold mb-6">
-        Semua Postingan 📝
+        Semua Blog 📝
       </h1>
 
       
