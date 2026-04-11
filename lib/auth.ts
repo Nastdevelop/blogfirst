e31f99291a6f5b26d24de977de39525adc96300c
Nastdevelop <nastdeveloper@gmail.com>
2026-04-11 23:25:55 +0700

    finishing a error

--- lib/auth.ts
+++ lib/auth.ts
@@ -49,14 +49,15 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async jwt({ token, user }) {
+      // Saat login pertama, user object tersedia
       if (user) {
         token.id = user.id;
       }
       return token;
     },
-  
     async session({ session, token }) {
-      if (session.user) {
+      // Teruskan id dari token ke session
+      if (token && session.user) {
         session.user.id = token.id as string;
       }
       return session;
